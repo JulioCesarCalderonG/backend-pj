b9b56648df041872c6454636464554a7e55f4a79
julio cesar calderon galindo <julcescalgalty@gmail.com>
2023-05-22 14:05:03 -0500

    subiendo datos

--- controllers/areas.js
+++ controllers/areas.js
@@ -1,26 +1,43 @@
-const { request, response } = require("express");
+const { request, response } = require('express');
+const { Area } = require('../models');
 
-
-const mostrarAreas = (req=request,res=response)=>{
-
-}
-
-const agregarArea = (req=request,res=response)=>{
-
-}
-
-const modificarArea = (req=request,res=response)=>{
-    
-}
-
-const eliminarArea = (req=request,res=response)=>{
-    
-}
-
-module.exports = {
-    mostrarAreas,
-    agregarArea,
-    modificarArea,
-    eliminarArea
+const mostrarAreas = async(req = request, res = response) => {
+    const resp = await Area.findAll();
+    res.json({
+        ok:true,
+        msg:'Se muestran los datos correctamente',
+        resp
+    })
 };
 
+const agregarArea = async (req = request, res = response) => {
+  try {
+    const { nombre, sigla, ...data } = req.body;
+    data.nombre = nombre.toUpperCase();
+    data.sigla = sigla.toUpperCase();
+
+    const resp = await Area.create(data);
+
+    res.json({
+      ok: true,
+      msg: 'Datos ingresados correctamente',
+      resp,
+    });
+  } catch (error) {
+    res.status(400).json({
+        ok:false,
+        msg:`Error: ${error}`
+    })
+  }
+};
+
+const modificarArea = (req = request, res = response) => {};
+
+const eliminarArea = (req = request, res = response) => {};
+
+module.exports = {
+  mostrarAreas,
+  agregarArea,
+  modificarArea,
+  eliminarArea,
+};
